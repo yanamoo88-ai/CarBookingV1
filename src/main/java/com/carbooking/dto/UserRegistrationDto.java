8cbf66326df73393885cfa8226221e03ab35eff6
Vitali Romanski <vitromansky@gmail.com.>
2026-03-25 19:54:16 +0100

    add new all v1

--- src/main/java/com/carbooking/dto/UserRegistrationDto.java
+++ src/main/java/com/carbooking/dto/UserRegistrationDto.java
@@ -5,10 +5,13 @@ package com.carbooking.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.ToString;
+
 
 @Data
 @Builder
 @AllArgsConstructor
+@ToString(of = {"email"}) // Lombok сам создаст короткий toString, используя только email
 public class UserRegistrationDto {
 //    @JsonProperty("full_name")
     private String full_name;
